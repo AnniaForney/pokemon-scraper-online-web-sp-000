d7166976e726a0319735d7e5af155bf7316464e6
Annia Forney <anniaforney@gmail.com>
2020-08-27 16:19:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
+  end
+
+  def self.find
+    sql = <<-SQL
+    SELECT *
+    FROM pokemon WHERE id = ?
+  SQL
+
+  DB[:conn].execute(sql)
+  end
+
 end
